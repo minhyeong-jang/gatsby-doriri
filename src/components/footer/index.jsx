8d267a39f751cf0747b0353cd156b6df84e53e78
Noah <savedream75@gmail.com>
2019-05-22 17:34:44 +0900

    Upload Expo-Libary & index

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,7 +4,7 @@ import './index.scss'
 
 export const Footer = () => (
   <footer className="footer">
-    ©<a href="https://github.com/JaeYeopHan">Doriri</a>, Built with{' '}
+    ©<a href="https://github.com/minhyeong-jang">Doriri</a>, Built with{' '}
     <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">
       Gatsby-starter-bee
     </a>
